03567c7fb5540d8a2ce499f61c09e038b51ded11
yuhwanwoo <6514687@naver.com>
2021-11-18 21:58:31 +0900

    prj mod

--- lunchpick/lunchpick/src/main/java/com/lunchpick/lunchpick/controller/Home2Controller.java
+++ lunchpick/lunchpick/src/main/java/com/lunchpick/lunchpick/controller/Home2Controller.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class Home2Controller {
 
     @RequestMapping("/")
     public String home() {
